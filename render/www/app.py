135d66649cfa99c30736d615a747128a1c52f797
manhdoi <manhtran40kc@gmail.com>
2024-05-29 23:05:10 +0700

    refactor js to jsv

--- render/www/app.py
+++ render/www/app.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from flask_login import LoginManager
 
 from render.models.user import User
--- render/www/webpack.config.js
+++ render/www/webpack.config.js
@@ -7,9 +7,9 @@ const BUILD_DIR = path.resolve(__dirname, './static/render/dist');
 
 module.exports = {
     entry: {
-        index: [`${JS_DIR}/index.js`],
-        login: [`${JS_DIR}/login.js`],
-        register: [`${JS_DIR}/register.js`],
+        index: [`${JS_DIR}/index.jsx`],
+        login: [`${JS_DIR}/login.jsx`],
+        register: [`${JS_DIR}/register.jsx`],
         list_view: [`${JS_DIR}/list_view.jsx`]
     },
     output: {
