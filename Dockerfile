29090f46dada8f95afd9695c0de4f2b5a4d567b0
manhdoi <manhtran40kc@gmail.com>
2024-09-01 23:30:18 +0700

    multi stage build

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,35 @@
-FROM python:3.12.4
+FROM node:slim AS node_builder
+
+WORKDIR /opt/app
+
+COPY . ./
+
+RUN cd render/www \
+    && npm i \
+    && npm run build
+
+CMD []
+
+
+FROM python:3.12.4 AS render_app
 
 EXPOSE 5000
 
 WORKDIR /opt/app
 
-RUN apt update \
-    && apt install nodejs -y \
-    && apt install npm -y
+#RUN apt update \
+#    && apt install nodejs -y \
+#    && apt install npm -y
+
+RUN pip install build
 
 COPY . ./
 
-RUN python setup.py compile_assert install clean
+COPY --from=node_builder /opt/app/render/www/static/render/dist /opt/app/render/www/static/render/dist
+
+#RUN python setup.py compile_assert install clean
+
+RUN python -m build \
+    && pip install dist/render-0.0.1-py3-none-any.whl
 
 CMD []
--- setup.py
+++ setup.py
@@ -1,89 +1,16 @@
-import subprocess
-import typing
-import glob
+import os
 from pathlib import Path
 
-from setuptools import setup, Command
-import logging
-
-import os
+from setuptools import setup
 
 name = "render"
+version = "0.0.1"
 SOURCES_ROOT = Path(__file__).parent.resolve()
-
-
-class CompileAssert(Command):
-    user_options: typing.List[str] = []
-
-    def initialize_options(self) -> None:
-        pass
-
-    def finalize_options(self) -> None:
-        pass
-
-    def run(self) -> None:
-        www_dir = SOURCES_ROOT / "render" / "www"
-        # subprocess.check_call(["yarn", "config", "set", "strict-ssl", "false", "-g"], cwd=str(www_dir))
-        subprocess.check_call(["npm", "install", "--frozen-lockfile"], cwd=str(www_dir))
-        subprocess.check_call(["npm", "run", "build"], cwd=str(www_dir))
-
-
-class CleanCommand(Command):
-    user_options: typing.List[str] = []
-
-    def initialize_options(self) -> None:
-        pass
-
-    def finalize_options(self) -> None:
-        pass
-
-    @staticmethod
-    def rm_all_files(file) -> None:
-        files = glob.glob(file)
-        stack = files
-        files_sorted = []
-
-        def rec(stk):
-            while stk:
-                f = stk.pop()
-                fs = glob.glob(f"{f}/*", recursive=True)
-                for i in fs:
-                    stk.append(i)
-                    rec(stk)
-                files_sorted.append(f)
-            return
-
-        rec(stack)
-
-        for x in files_sorted:
-            try:
-                logging.info(f"Removing {x}")
-                if os.path.isfile(x):
-                    os.remove(x)
-                else:
-                    os.rmdir(x)
-            except Exception as e:
-                logging.warning(f"Error when removing {x}")
-                logging.exception(e)
-
-    def run(self) -> None:
-        os.chdir(str(SOURCES_ROOT))
-        self.rm_all_files("./build/*")
-        self.rm_all_files("./build")
-        self.rm_all_files("./.pytest_cache/*")
-        self.rm_all_files("./*.egg-info/*")
-        self.rm_all_files("./*.egg-info")
-        self.rm_all_files("./dist/*")
-        self.rm_all_files("./dist")
-        self.rm_all_files('./**/__pycache__/*')
-        self.rm_all_files('./**/*.pyc')
 
 
 def do_setup():
     packages = [x[0].replace("./", "").replace("/", ".") for x in
                 filter(lambda x: x[2].__contains__("__init__.py"), os.walk("./"))]
-
-    version = next(line for line in open("version.txt", "r")).strip()
 
     setup(
         name=name,
@@ -100,10 +27,7 @@ def do_setup():
                           "Flask-WTF==1.2.1",
                           "croniter==2.0.5",
                           "Flask-Login==0.6.3"],
-        cmdclass={
-            'clean': CleanCommand,
-            'compile_assert': CompileAssert
-        }
+        cmdclass={}
     )
 
 
